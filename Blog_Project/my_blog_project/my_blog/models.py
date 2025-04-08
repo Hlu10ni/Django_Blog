577e953abe4fff0869b61bf3ab4e49a707ad04f2
Hlulani Hlungwane <hlulanihlungwane56@gmail.com>
2025-04-07 17:13:00 -0700

    Blog

--- Blog_Project/my_blog_project/my_blog/models.py
+++ Blog_Project/my_blog_project/my_blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+#Creating our Database
+
+class Post(models.Model):
+    # My table Fields
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=1000000)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
